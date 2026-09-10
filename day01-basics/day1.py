e3a8f46111e8043235ebf73784f4c48e0fc9b147
Vasyl <vstratulatus@ukr.net>
2026-09-10 14:29:16 +0100

    Add Python basics practice

---
+++ day01-basics/day1.py
@@ -0,0 +1,14 @@
+server_name = "wed-server"
+port = 8080
+cpu_load = 47.5
+server_active = True
+
+print(server_name)
+print(port)
+print(cpu_load)
+print(server_active)
+
+print(type(server_name))
+print(type(port))
+print(type(cpu_load))
+print(type(server_active))
