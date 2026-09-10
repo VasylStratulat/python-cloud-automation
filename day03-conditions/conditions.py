d72bd121a35e1532089b28cee8573e8a7954822b
Vasyl <vstratulatus@ukr.net>
2026-09-10 14:31:19 +0100

    Add Python conditions practice

---
+++ day03-conditions/conditions.py
@@ -0,0 +1,6 @@
+is_logged_in = True
+
+if not is_logged_in:
+	print("Please log in")
+else:
+	print("Welcom")
---
+++ day03-conditions/day3.py
@@ -0,0 +1,36 @@
+a = 10
+b =  3
+
+summa = a + b
+difference = a - b
+product = a * b
+division = a / b
+floor_division = a // b
+remainder = a % b
+power = a ** b
+
+print(summa)
+print(difference)
+print(product)
+print(division) 
+print(floor_division)
+print(remainder)
+print(power)
+
+x = 10
+y = 5
+
+print(bool(x > y))
+print(bool(x < y))
+print(bool(x == y))
+print(bool(x != y))
+print(bool(x >= 10))
+print(bool(y <= 5))
+
+age = 25
+is_student = True
+
+print(bool(age > 18 and age <= 65))
+print(bool(age <18 or age > 65))
+print(bool(is_student == True))
+print(bool(not is_student))
