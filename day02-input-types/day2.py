2c5ea0b7ba3e6fbbfa7f08a9203f4bf9208953dd
Vasyl <vstratulatus@ukr.net>
2026-09-10 14:30:17 +0100

    Add Python input and type practice

---
+++ day02-input-types/day2.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+salary = float(input("Enter your salary: "))
+
+print(name)
+print(age)
+print(salary)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+
+
