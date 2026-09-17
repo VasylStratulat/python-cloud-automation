b89f5e88831365c8cad22f7603b0822a880766a0
Vasyl <vstratulatus@ukr.net>
2026-09-17 14:54:06 +0100

    Practice Python line based file operations

--- day09-files/files.py
+++ day09-files/files.py
@@ -18,3 +18,27 @@ with open("test.txt", "w") as file:
 
 with open("test.txt", "a") as file:
     file.write("Second line\n")
+
+
+with open("servers.txt", "r") as file:
+    lines = file.readlines()
+
+servers = []
+
+for line in lines:
+    servers.append(line.strip())
+
+print(servers)
+
+
+with open("clean_servers.txt", "w") as file:
+    for server in servers:
+        file.write(server + "\n")
+
+server_lines = []
+
+for server in servers:
+    server_lines.append(server + "\n")
+
+with open("servers_copy.txt", "w") as file:
+    file.writelines(server_lines)
