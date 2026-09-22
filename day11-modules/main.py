213bfbda322abbb7e00a6261db44ec60eec213ad
Vasyl <vstratulatus@ukr.net>
2026-09-22 13:12:17 +0100

    Practice Python module imports

---
+++ day11-modules/main.py
@@ -0,0 +1,9 @@
+from math_tools import add_numbers, multiply_numbers
+
+print(add_numbers(10, 7))
+print(multiply_numbers(4, 5))
+
+import math_tools as mt
+
+print(mt.add_numbers(6, 4))
+print(mt.multiply_numbers(3, 5))
